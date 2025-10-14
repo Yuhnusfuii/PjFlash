f7f9da6319df10775ee90d7f27e84959cd3d872e
Yuhnusfuii <huynguyennhat.0209@gmail.com>
2025-10-14 18:43:21 +0700

    Update livewire.php

--- config/livewire.php
+++ config/livewire.php
@@ -14,7 +14,7 @@ return [
     */
 
     'class_namespace' => 'App\\Livewire',
-
+    'asset_url' => env('ASSET_URL', null),
     /*
     |---------------------------------------------------------------------------
     | View Path
